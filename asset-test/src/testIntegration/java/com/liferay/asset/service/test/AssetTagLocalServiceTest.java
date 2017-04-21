4fe99b0836488af3e012c2fbed624751db319ba6
Juergen Kappler <jurgen.kappler@liferay.com>
2017-04-21 11:47:04 +0200

    LPS-72096 Move tests that can be moved, to asset-tests module, no logical changes

---
+++ asset-test/src/testIntegration/java/com/liferay/asset/service/test/AssetTagLocalServiceTest.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.service.test;
+
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.model.AssetTagStats;
+import com.liferay.asset.kernel.service.AssetTagLocalServiceUtil;
+import com.liferay.asset.kernel.service.AssetTagStatsLocalServiceUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ListTypeConstants;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.OrganizationConstants;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Michael C. Han
+ * @author Manuel de la Peña
+ */
+@Sync
+public class AssetTagLocalServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_organizationIndexer = IndexerRegistryUtil.getIndexer(
+			Organization.class);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if (_organizationIndexer != null) {
+			IndexerRegistryUtil.register(_organizationIndexer);
+		}
+	}
+
+	@Test
+	public void testDeleteTag() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		AssetTag assetTag = AssetTagLocalServiceUtil.addTag(
+			TestPropsValues.getUserId(), _group.getGroupId(), "Tag",
+			serviceContext);
+
+		serviceContext.setAssetTagNames(new String[] {assetTag.getName()});
+
+		_organization = OrganizationLocalServiceUtil.addOrganization(
+			TestPropsValues.getUserId(),
+			OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID,
+			RandomTestUtil.randomString(),
+			OrganizationConstants.TYPE_ORGANIZATION, 0, 0,
+			ListTypeConstants.ORGANIZATION_STATUS_DEFAULT,
+			RandomTestUtil.randomString(), true, serviceContext);
+
+		TestAssetIndexer testAssetIndexer = new TestAssetIndexer();
+
+		testAssetIndexer.setExpectedValues(
+			Organization.class.getName(), _organization.getOrganizationId());
+
+		if (_organizationIndexer == null) {
+			_organizationIndexer = IndexerRegistryUtil.getIndexer(
+				Organization.class);
+		}
+
+		IndexerRegistryUtil.register(testAssetIndexer);
+
+		AssetTagLocalServiceUtil.deleteTag(assetTag);
+
+		Assert.assertNull(
+			AssetTagLocalServiceUtil.fetchAssetTag(assetTag.getTagId()));
+
+		long classNameId = PortalUtil.getClassNameId(Organization.class);
+
+		AssetTagStats assetTagStats = AssetTagStatsLocalServiceUtil.getTagStats(
+			assetTag.getTagId(), classNameId);
+
+		Assert.assertEquals(0, assetTagStats.getAssetCount());
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@DeleteAfterTestRun
+	private Organization _organization;
+
+	private Indexer<Organization> _organizationIndexer;
+
+}
