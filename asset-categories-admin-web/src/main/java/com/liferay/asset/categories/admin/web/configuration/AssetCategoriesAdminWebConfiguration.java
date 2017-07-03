ed016e43ff579cdb82c7bdfcd178a76cf78dfd08
Juergen Kappler <jurgen.kappler@liferay.com>
2017-07-03 12:42:09 +0200

    LPS-73449 Remove configuration option since it's no longer used

--- asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/configuration/AssetCategoriesAdminWebConfiguration.java
+++ asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/configuration/AssetCategoriesAdminWebConfiguration.java
@@ -30,20 +30,6 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 public interface AssetCategoriesAdminWebConfiguration {
 
-	/**
-	 * Set this to <code>true</code> to enable the asset categories tree to
-	 * display as a flattened tree.
-	 *
-	 * @return <code>true</code> if the asset categories tree is displayed as a
-	 * flattened tree; <code>false</code> otherwise
-	 */
-	@Meta.AD(
-		deflt = "false",
-		description = "flattened.navigation.allowed.description",
-		required = false
-	)
-	public boolean flattenedNavigationAllowed();
-
 	@Meta.AD(
 		deflt = AssetCategoriesNavigationDisplayStyleKeys.DEFAULT,
 		name = "category-navigation-display-style",
